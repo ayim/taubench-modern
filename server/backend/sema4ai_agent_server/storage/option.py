f5b858c1eb2c9a924085da0d053bbe7c1e142943
Mikko Korpela <mikko@sema4.ai>
2024-07-29 09:45:03 +0300

    conditional importing of postgres and sqlite storage

--- server/backend/sema4ai_agent_server/storage/option.py
+++ server/backend/sema4ai_agent_server/storage/option.py
@@ -1,20 +1,35 @@
 import os
+from typing import Callable, Dict, Final
 
 from sema4ai_agent_server.storage import BaseStorage
-from sema4ai_agent_server.storage.postgres import PostgresStorage
-from sema4ai_agent_server.storage.sqlite import SqliteStorage
 
-_storage = None
+
+def get_postgres_storage() -> BaseStorage:
+    from sema4ai_agent_server.storage.postgres import PostgresStorage
+    return PostgresStorage()
+
+
+def get_sqlite_storage() -> BaseStorage:
+    from sema4ai_agent_server.storage.sqlite import SqliteStorage
+    return SqliteStorage()
+
+
+STORAGE_TYPES: Final[Dict[str, Callable[[], BaseStorage]]] = {
+    "postgres": get_postgres_storage,
+    "sqlite": get_sqlite_storage
+}
+
+
+def initialize_storage() -> BaseStorage:
+    db_type = os.environ.get("S4_AGENT_SERVER_DB_TYPE", "sqlite")
+    storage_func = STORAGE_TYPES.get(db_type)
+    if storage_func is None:
+        raise ValueError(f"Invalid storage type: {db_type}")
+    return storage_func()
+
+
+STORAGE: Final[BaseStorage] = initialize_storage()
 
 
 def get_storage() -> BaseStorage:
-    db_type = os.environ.get("S4_AGENT_SERVER_DB_TYPE", "sqlite")
-    global _storage
-    if _storage is None:
-        if db_type == "postgres":
-            _storage = PostgresStorage()
-        elif db_type == "sqlite":
-            _storage = SqliteStorage()
-        else:
-            raise ValueError(f"Invalid storage type: {db_type}")
-    return _storage
+    return STORAGE
