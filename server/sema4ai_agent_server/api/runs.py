573080d2858338b607ca9ae4d5d6b8aeb4b6b986
P. Taylor Goetz <ptgoetz@gmail.com>
2024-08-22 16:20:28 -0400

    Implement StreamRequest as input for '/api/v1/runs/' endpoints.

--- server/sema4ai_agent_server/api/runs.py
+++ server/sema4ai_agent_server/api/runs.py
@@ -9,6 +9,7 @@ from langchain_core.messages import AnyMessage
 from langchain_core.runnables import RunnableConfig
 from langserve.server import _unpack_input
 from langsmith.utils import tracing_is_enabled
+from orjson import orjson
 from pydantic import BaseModel, Field
 from sse_starlette import EventSourceResponse
 
@@ -18,6 +19,7 @@ from sema4ai_agent_server.langsmith_client import (
     get_langsmith_thread_url,
     save_langsmith_thread_url,
 )
+from sema4ai_agent_server.schema import StreamRequest
 from sema4ai_agent_server.storage.option import get_storage
 from sema4ai_agent_server.stream import astream_state, invoke_state, to_sse
 
@@ -25,16 +27,7 @@ router = APIRouter()
 langsmith_client = langsmith.client.Client() if tracing_is_enabled() else None
 
 
-class CreateRunPayload(BaseModel):
-    """Payload for creating a run."""
-
-    thread_id: str
-    input: Optional[Union[Sequence[AnyMessage], Dict[str, Any]]] = Field(
-        default_factory=dict
-    )
-
-
-async def _run_input_and_config(payload: CreateRunPayload, user_id: str):
+async def _run_input_and_config(payload: StreamRequest, user_id: str):
     thread = await get_storage().get_thread(user_id, payload.thread_id)
     if not thread:
         raise HTTPException(status_code=404, detail="Thread not found")
@@ -69,22 +62,8 @@ async def _run_input_and_config(payload: CreateRunPayload, user_id: str):
         },
     }
 
-    try:
-        input_ = (
-            _unpack_input(
-                runnable_agent.get_input_schema(config).validate(payload.input)
-            )
-            if payload.input is not None
-            else None
-        )
-        input_ = {"messages": input_ or []}
-    except ValidationError as e:
-        raise RequestValidationError(e.errors(), body=payload)
-
+    input_ = {"messages": payload.get_langchain_messages()}
     return input_, config, thread, agent
-
-
-runs = {}
 
 
 async def background_invoke(input_, config):
@@ -95,7 +74,7 @@ async def background_invoke(input_, config):
 
 @router.post("/async_invoke")
 async def create_run(
-    payload: CreateRunPayload,
+    payload: StreamRequest,
     user: AuthedUser,
     background_tasks: BackgroundTasks,
 ):
@@ -121,9 +100,9 @@ async def get_run_status(rid: str):
 
 @router.post("/stream")
 async def stream_run(
-    payload: CreateRunPayload,
+    payload: StreamRequest,
     user: AuthedUser,
-):
+) -> EventSourceResponse:
     """Create a run."""
     input_, config, thread, _ = await _run_input_and_config(payload, user.user_id)
     if langsmith_client:
@@ -134,7 +113,7 @@ async def stream_run(
 
 @router.post("/invoke")
 async def invoke_run(
-    payload: CreateRunPayload,
+    payload: StreamRequest,
     user: AuthedUser,
 ):
     """Create a run."""
