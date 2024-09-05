3936ae90d94250e76ad881ad97f858159f5f9747
mstn <mstn@posteo.org>
2024-09-05 12:01:57 +0200

    include spec obj in distro

--- server/agent-server-interface/src/index.ts
+++ server/agent-server-interface/src/index.ts
@@ -1,2 +1,2 @@
 export * from "./schema";
-export * from "./meta";
+export * from "./spec.gen";
--- server/agent-server-interface/src/meta.ts
+++
@@ -1,4 +0,0 @@
-export const meta = {
-  "title": "Sema4.ai Agent Server API",
-  "version": "0.3.21"
-};
