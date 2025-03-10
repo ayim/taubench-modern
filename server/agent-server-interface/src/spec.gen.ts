66e38e2c4fceb253387fdeb283e5ab3745a78ca9
Sema4.ai Agent Team <agent-team@no-reply.sema4.ai>
2025-03-10 20:51:07 +0000

    bump version 1.1.4-alpha.87 --> 1.1.4-alpha.88

--- server/agent-server-interface/src/spec.gen.ts
+++ server/agent-server-interface/src/spec.gen.ts
@@ -6,7 +6,7 @@ export const spec = {
   openapi: "3.1.0",
   info: {
     title: "Sema4.ai Agent Server Private API Version 1",
-    version: "1.1.4-alpha.87",
+    version: "1.1.4-alpha.88",
   },
   paths: {
     "/api/v1/ok": {
