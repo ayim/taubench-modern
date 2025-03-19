b3a3e39e2a6ec0d5fe4fe4a62d0ad71c45a54bab
P. Taylor Goetz <ptgoetz@gmail.com>
2025-03-18 21:16:39 -0400

    Prep for v1.2.0-beta release (#448)
    * Prep for v1.2.0-beta release
    * regen ASI

--- server/agent-server-interface/src/spec.gen.ts
+++ server/agent-server-interface/src/spec.gen.ts
@@ -6,7 +6,7 @@ export const spec = {
   openapi: "3.1.0",
   info: {
     title: "Sema4.ai Agent Server Private API Version 1",
-    version: "1.1.4-alpha.91",
+    version: "1.2.0-alpha",
   },
   paths: {
     "/api/v1/ok": {
