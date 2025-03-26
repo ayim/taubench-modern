31d1e99e4080b99dd8fd08863dac3d55c8787bc4
Jordan Henkel <jordan@sema4.ai>
2025-03-26 14:48:16 -0500

    Fix dependency hell

--- server/tests/unit_tests/sema4ai_agent_server/helpers.py
+++ server/tests/unit_tests/sema4ai_agent_server/helpers.py
@@ -1,6 +1,6 @@
 from contextlib import asynccontextmanager
 
-from httpx import AsyncClient
+from httpx import ASGITransport, AsyncClient
 from typing_extensions import AsyncGenerator
 
 
@@ -9,5 +9,5 @@ async def get_client() -> AsyncGenerator[AsyncClient, None]:
     """Get the app."""
     from sema4ai_agent_server.server import app
 
-    async with AsyncClient(app=app, base_url="http://test") as ac:
+    async with AsyncClient(transport=ASGITransport(app), base_url="http://test") as ac:
         yield ac
