39e6d829ddc580dbe16e961e6abccccbf776e90b
Cosmin G. Popa <cpopa@sema4.ai>
2025-08-15 12:13:27 +0300

    Removing LLM Utils  (#652)

--- packages/typescript-llm-utils/src/openAI/types.ts
+++
@@ -1,9 +0,0 @@
-import * as ASInterface from '@sema4ai/agent-server-interface';
-
-export type OpenAIModelInfo = ASInterface.components['schemas']['OpenAIGPT'];
-
-export type OpenAILLMConfiguration = {
-  provider: 'OpenAI';
-  model: 'gpt-3.5-turbo-1106' | 'gpt-4-turbo' | 'gpt-4o' | 'gpt-4o-mini';
-  config: Omit<OpenAIModelInfo['config'], 'temperature'>;
-};
