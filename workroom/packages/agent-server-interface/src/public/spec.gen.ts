4e1eb1264225e69da0f8b5647b6bc18e689770cd
sema4-ci-user <167439670+sema4-ci-user@users.noreply.github.com>
2025-09-08 13:08:49 +0300

    Publish `agent-server-interface` v2.1.0 (#846)

--- workroom/packages/agent-server-interface/src/public/spec.gen.ts
+++ workroom/packages/agent-server-interface/src/public/spec.gen.ts
@@ -6,7 +6,7 @@ export const spec = {
   openapi: '3.1.0',
   info: {
     title: 'Sema4.ai Agent Server Public API Version 2',
-    version: '2.0.42',
+    version: '2.1.0',
   },
   paths: {
     '/api/public/v1/agents': {
