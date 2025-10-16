01a0071e4d9c3b426e4aa1fc5699fc74ad14caa2
Karri Kuivanen <karri@sema4.ai>
2025-10-16 16:29:30 +0300

    chore: Update SPAR base images, enhance base img update script (#1296)

--- scripts/get-fresh-spar-base-images.sh
+++ scripts/get-fresh-spar-base-images.sh
@@ -2,12 +2,25 @@
 
 set -euo pipefail
 
-script_dir="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
+repo_root="$(git rev-parse --show-toplevel)"
+dockerfile_path="${repo_root}/Dockerfile.spar"
 
-for img in $(grep -E "ARG (GOLANG|NODEJS)_BASE_IMAGE=" "${script_dir}/../Dockerfile.spar" | cut -d= -f2 | tr -d '"' | cut -d@ -f1); do
-  digest="$(
-    docker pull --platform linux/amd64 -q "${img}" \
+for stale_ref in $(grep -E "ARG (GOLANG|NODEJS)_BASE_IMAGE=" "${dockerfile_path}" | cut -d= -f2 | tr -d '"'); do
+  ref_without_digest="$(echo "${stale_ref}" | cut -d@ -f1)"
+  latest_digest="$(
+    docker pull --platform linux/amd64 -q "$(echo "${stale_ref}" | cut -d@ -f1)" \
       | xargs docker inspect --format='{{index .RepoDigests 0}}' | cut -d@ -f2
   )"
-  echo "${img}@${digest}"
+  updated_ref="${ref_without_digest}@${latest_digest}"
+  if [[ "${stale_ref}" != "${updated_ref}" ]]; then
+    tmp_file="$(mktemp)"
+    sed -e "s/${stale_ref}/${updated_ref}/" "${dockerfile_path}" > "${tmp_file}"
+    mv "${tmp_file}" "${dockerfile_path}"
+    echo "$(tput bold)Updated:$(tput sgr0)"
+    echo "   ${stale_ref}"
+    echo "=> ${updated_ref}"
+  else
+    echo "$(tput bold)Already fresh:$(tput sgr0)"
+    echo "   ${updated_ref}"
+  fi
 done
