49a01e9dfb7568164cade976666e4d13fb0e7bbd
Valters Stuķēns <valters@sema4.ai>
2025-10-30 10:46:27 +0200

    fix: content scrolling for workroom pages (#1453)

--- workroom/frontend/src/components/layout/Page.tsx
+++ workroom/frontend/src/components/layout/Page.tsx
@@ -11,15 +11,17 @@ type Props = {
 
 export const Page: FC<Props> = ({ actions, children, title, icon: Icon }) => {
   return (
-    <Box as="section" py="$64" px={['$16', '$16', '$16', '$40']} maxWidth={1280} width="100%" mx="auto">
-      <Box display="flex" justifyContent="space-between" alignItems="center" mb="$20">
-        <Box display="flex" gap="$16">
-          {Icon && <Icon size={36} />}
-          <Typography variant="display-large">{title}</Typography>
+    <Box overflow="auto">
+      <Box as="section" py="$64" px={['$16', '$16', '$16', '$40']} maxWidth={1280} width="100%" mx="auto">
+        <Box display="flex" justifyContent="space-between" alignItems="center" mb="$20">
+          <Box display="flex" gap="$16">
+            {Icon && <Icon size={36} />}
+            <Typography variant="display-large">{title}</Typography>
+          </Box>
+          {actions}
         </Box>
-        {actions}
+        {children}
       </Box>
-      {children}
     </Box>
   );
 };
