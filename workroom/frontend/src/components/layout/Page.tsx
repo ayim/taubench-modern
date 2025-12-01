142b6e3b6a31bbbe9bf301596aeecc71b006a34c
Rihards Gravis <rihards@sema4.ai>
2025-12-01 12:02:10 +0200

    CLOUD-5610: Spar UI layout fix (#1806)

--- workroom/frontend/src/components/layout/Page.tsx
+++ workroom/frontend/src/components/layout/Page.tsx
@@ -11,8 +11,8 @@ type Props = {
 
 export const Page: FC<Props> = ({ actions, children, title, icon: Icon }) => {
   return (
-    <Box overflow="auto">
-      <Box as="section" py="$64" px={['$16', '$16', '$16', '$40']} maxWidth={1280} width="100%" mx="auto">
+    <Box as="section" overflow="auto">
+      <Box py="$64" px={['$16', '$16', '$16', '$40']} maxWidth={1280} width="100%" mx="auto">
         <Box display="flex" justifyContent="space-between" alignItems="center" mb="$20">
           <Box display="flex" gap="$16">
             {Icon && <Icon size={36} />}
