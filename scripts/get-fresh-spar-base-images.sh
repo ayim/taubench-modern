2b513628bbc1b3cc00cee6cbf6224ae1936dfb99
Sampo Ahokas <sampo@sema4.ai>
2026-01-19 15:13:04 +0200

    Update docker base images (#2172)
    What?
    - Update base images of `spar` and `mcp-runtime`
    - Update base image update script to also address mcp-runtime and fix warnings about hardcoded platform setting
    
    Why?
    Dockerfiles for spar and mcp-runtime are using outdated docker base images containing CVEs.

--- scripts/get-fresh-spar-base-images.sh
+++ scripts/get-fresh-spar-base-images.sh
@@ -3,24 +3,32 @@
 set -euo pipefail
 
 repo_root="$(git rev-parse --show-toplevel)"
-dockerfile_path="${repo_root}/Dockerfile.spar"
 
-for stale_ref in $(grep -E "ARG (GOLANG|NODEJS)_BASE_IMAGE=" "${dockerfile_path}" | cut -d= -f2 | tr -d '"'); do
-  ref_without_digest="$(echo "${stale_ref}" | cut -d@ -f1)"
-  latest_digest="$(
-    docker pull --platform linux/amd64 -q "$(echo "${stale_ref}" | cut -d@ -f1)" \
-      | xargs docker inspect --format='{{index .RepoDigests 0}}' | cut -d@ -f2
-  )"
-  updated_ref="${ref_without_digest}@${latest_digest}"
-  if [[ "${stale_ref}" != "${updated_ref}" ]]; then
-    tmp_file="$(mktemp)"
-    sed -e "s/${stale_ref}/${updated_ref}/" "${dockerfile_path}" > "${tmp_file}"
-    mv "${tmp_file}" "${dockerfile_path}"
-    echo "$(tput bold)Updated:$(tput sgr0)"
-    echo "   ${stale_ref}"
-    echo "=> ${updated_ref}"
-  else
-    echo "$(tput bold)Already fresh:$(tput sgr0)"
-    echo "   ${updated_ref}"
-  fi
+dockerfiles=(
+  "${repo_root}/Dockerfile.spar"
+  "${repo_root}/workroom/mcp-runtime/Dockerfile"
+)
+
+for dockerfile_path in "${dockerfiles[@]}"; do
+  echo "$(tput bold)Processing:$(tput sgr0) ${dockerfile_path#"${repo_root}/"}"
+  for stale_ref in $(grep -E "ARG (GOLANG|NODEJS)_BASE_IMAGE=" "${dockerfile_path}" | cut -d= -f2 | tr -d '"'); do
+    ref_without_digest="$(echo "${stale_ref}" | cut -d@ -f1)"
+    latest_digest="$(
+      docker pull --platform linux/amd64 -q "$(echo "${stale_ref}" | cut -d@ -f1)" \
+        | xargs docker inspect --format='{{index .RepoDigests 0}}' | cut -d@ -f2
+    )"
+    updated_ref="${ref_without_digest}@${latest_digest}"
+    if [[ "${stale_ref}" != "${updated_ref}" ]]; then
+      tmp_file="$(mktemp)"
+      sed -e "s/${stale_ref}/${updated_ref}/" "${dockerfile_path}" > "${tmp_file}"
+      mv "${tmp_file}" "${dockerfile_path}"
+      echo "  $(tput bold)Updated:$(tput sgr0)"
+      echo "     ${stale_ref}"
+      echo "  => ${updated_ref}"
+    else
+      echo "  $(tput bold)Already fresh:$(tput sgr0)"
+      echo "     ${updated_ref}"
+    fi
+  done
+  echo ""
 done
